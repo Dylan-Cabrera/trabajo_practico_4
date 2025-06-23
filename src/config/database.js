e3a26bcbe4718fd943f54bc327a311c7ed9716df
DylanC <cabreradylan055@gmail.com>
2025-06-23 11:51:59 -0300

    se añadio la configuración de la base de datos

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+);
+
+export const startBD = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Se conecto a la base de datos")
+        await sequelize.sync();
+    } catch(error) {
+        console.log(`Hubo un error al conectar a la base de datos ${error}`)
+    }
+}
